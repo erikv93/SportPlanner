9f1adaefa4c507592ef647bebfdc3e7f486bea01
erikv93 <erik.voors@gmail.com>
2026-05-09 10:52:09 +0200

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
+
 const dev = process.env.NODE_ENV === 'development';
+
+const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
+const base = process.env.GITHUB_PAGES === 'true' && repo ? `/${repo}` : '';
 
 export default {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.GITHUB_PAGES ? '/REPO' : ''
+      base
     }
   }
 };
